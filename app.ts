bc4433ab4398b34f953eef6e4910e92513ff216d
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-03 15:02:08 +0500

    first 4 programecompleted

--- app.ts
+++ app.ts
@@ -1,3 +1,27 @@
-console.log("====================================");
-console.log("Abc");
-console.log("====================================");
+/**
+ * It takes no arguments and returns nothing.
+ * Taking an input in prompt and showing a greeting message in console of browser
+ */
+const personalMessage = (): void => {
+  let name: string | null;
+  do {
+    name = prompt("Enter your name please.");
+  } while (name === null || name === "");
+  console.log(
+    `Assalam u Alaikum ${name}! would You like to learn some python today.`
+  );
+};
+const nameCases = (): void => {
+  let name: string | null;
+  do {
+    name = prompt("Enter your name please.");
+  } while (name === null || name === "");
+  console.log(`Your name in lower case is "${name.toLowerCase()}"`);
+  console.log(`Your name in upper case is "${name.toUpperCase()}"`);
+};
+
+const famousQoute = (): void => {
+  console.log(
+    `Albert Einstein once said, “A person who never made a mistake never tried anything new.”`
+  );
+};
